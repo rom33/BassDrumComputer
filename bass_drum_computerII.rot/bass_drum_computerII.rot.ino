d91a3e228311077c2a4513c4be03db52244ff0b0
rom33 <49639367+rom33@users.noreply.github.com>
2019-12-25 11:45:49 +0100

    Add files via upload

--- bass_drum_computerII.rot/bass_drum_computerII.rot.ino
+++ bass_drum_computerII.rot/bass_drum_computerII.rot.ino
@@ -232,8 +232,10 @@ void loop() {
 while(1){
   playNotes();
   delay(250 * 60 / tempo);
+  if(tick%4){
   talkMIDI(0xB0, 0x7b, 127); //all notes channel 1 off
   talkMIDI(0xB1, 0x7b, 127); //all notes channel 2 off
+  }
   talkMIDI(0xB0, 0x0c, analogRead(A11)/8); // effect control 1 (sets global reverb decay)
   talkMIDI(0xB0, 0x26, analogRead(A10)/8); // RPN LSB: 0 = bend range
 //  VSWriteRegister(0x1e03, analogRead(A9), 0);
@@ -241,11 +243,6 @@ while(1){
   VSWriteRegister(0x0B, analogRead(A8)/16, analogRead(A9)/16); // Master Vol control left right
 //  VSWriteRegister(0x02, analogRead(A8)/16, analogRead(A9)/16);
 }
-}
-void DrawValuePan(int value,int height){
-  tft.drawFastVLine(320+value, height, 6, TFT_WHITE);
-  tft.drawFastVLine(321+value, height, 6, TFT_RED);
-  tft.drawFastVLine(322+value, height, 6, TFT_WHITE);
 }
 void printCopyTo()
 {
@@ -345,149 +342,6 @@ void readCalibrate() {
     file.close();
   }else calibrate();
 }
-void drawPattern()
-{
-   if(pat>7){
-      holeNote.draw(tft);
-      halfNote.draw(tft);
-      quaterNote.draw(tft);
-      eighthNote.draw(tft);
-      sixteenthNote.draw(tft);
-      tft.setCursor(90, 290);
-      tft.print("Sixteen Note");
-      noteLen=5;     
-   }else{
-      tft.fillRect(440, 160, 40, 150, TFT_BLUE);
-      tft.setCursor(90, 290);
-      tft.print("            ");
-      //      tft.fillRect(90, 290, 100, 8, TFT_BLUE);
-        }
-  for (x = 0; x < 16; x++) {
-    for (slope2 = 0; slope2 < 13; slope2++) {
-      tool = !((instrument[slope2][pat]) & (1 <<  x));
-      xx = (x * 16) + 14;
-      yy = 245 - (slope2 * 19);
-      DRAW;
-    }
-  }
-}
-
-void drawSong()
-{
-  for (x = 0; x < 16; x++) {
-    for (slope2 = 0; slope2 < 14; slope2++) {
-      tool = !((pattern[slope2 + pos] >> x) & (1));
-      xx = (x * 16) + 14;
-      yy = 264 - (slope2 * 19);
-      DRAW;
-    }
-  }
-}
-void drawRaster() {
-  rotMode1 =rotMode2 = rotMode3 = 0;
-  tft.fillScreen(TFT_BLUE);
-  StartStopButton.draw(tft);
-  TempMinusButton.draw(tft);
-  TempPlusButton.draw(tft);
-  tft.setCursor(320,27);
-  tft.print("Drums");
-  tft.setCursor(320,67);
-  tft.print("Bass");
-  tft.setCursor(320,107);
-  tft.print("Melody"); 
-  tft.drawRect(320,37,129,8,TFT_RED);
-  tft.drawRect(320,47,129,8,TFT_WHITE);
-  tft.fillRect(320,57,129,8,TFT_WHITE);
-  tft.drawRect(320,77,129,8,TFT_RED);
-  tft.drawRect(320,87,129,8,TFT_WHITE);
-  tft.fillRect(320,97,129,8,TFT_WHITE);  
-  tft.drawRect(320,117,129,8,TFT_RED);
-  tft.drawRect(320,127,129,8,TFT_WHITE);
-  tft.fillRect(320,137,129,8,TFT_WHITE);
-
-  tft.setCursor(455,128);
-  Format(reverb1);
-  tft.print(reverb1);
-  DrawValue(reverb1,128);
-  tft.setCursor(455,88);
-  Format(reverb2);
-  tft.print(reverb2);
-  DrawValue(reverb2,88);
-  tft.setCursor(455,48);
-  Format(reverb3);
-  tft.print(reverb3);
-  DrawValue(reverb3,48);  
-
-  tft.setCursor(455,38);
-  Format(Vol3);
-  tft.print(Vol3);
-  DrawValue(Vol3,38);
-  tft.setCursor(455,78);
-  Format(Vol2);
-  tft.print(Vol2);
-  DrawValue(Vol2,78);
-  tft.setCursor(455,118);
-  Format(Vol1);
-  tft.print(Vol1);
-  DrawValue(Vol1,118);   
-  
-  tft.setCursor(455,48);
-  Format(reverb3);
-  tft.print(reverb3);
-  DrawValue(reverb3,48);
-  tft.setCursor(455,88);
-  Format(reverb2);
-  tft.print(reverb2);
-  DrawValue(reverb2,88);
-  tft.setCursor(455,128);
-  Format(reverb1);
-  tft.print(reverb1);
-  DrawValue(reverb1,128); 
-
-  tft.setCursor(455,58);
-  Format(pan3-64);
-  tft.print(pan3-64);
-  DrawValuePan(pan3,58);
-  tft.setCursor(455,98);
-  Format(pan2-64);
-  tft.print(pan2-64);
-  DrawValuePan(pan2,98);
-  tft.setCursor(455,138);
-  Format(pan1-64);
-  tft.print(pan1-64);
-  DrawValuePan(pan1,138); 
-                 
-  ClearButton.draw(tft);
-  SaveButton.draw(tft);
-  Rewind.draw(tft);
-  Setup.draw(tft);
-  tft.setCursor(360, 150 );
-  tft.print(tempo);
-  tft.setCursor(385, 150 );
-  tft.print("bpm");
-  if (!mode) {
-    CopyButton.draw(tft);
-    PasteButton.draw(tft);
-    SongButton.draw(tft);
-    drawKeys();
-    tft.setCursor(275, 23 );
-    tft.print("Acc");
-  } else {
-    PatButton.draw(tft);
-    ScrollUp.draw(tft);
-    ScrollDown.draw(tft);
-  }
-  tft.setCursor(15, 5 );
-  tft.print("Bass & Drum-Computer with VS1053 DSP");
-  for (int x = 0; x < 17; x++) {
-    tft.drawFastVLine((x * 16) + 10, 14, 268, TFT_WHITE);
-    tft.drawFastVLine((x * 16) + 11, 14, 268, TFT_WHITE);
-  }
-  for (int x = 14; x < 286; x += 19) {
-    tft.drawFastHLine(10, x, 256, TFT_WHITE);
-    tft.drawFastHLine(10, x + 1, 256, TFT_WHITE);
-  }
-}
 void patternMode() {
   tick = -1;
   drawRaster();
@@ -544,17 +398,14 @@ void playNotes() {
         if (pat < 8) {
           channel = 9;
           note = patch[slope];
-//          talkMIDI(0xB0, 0x0c, reverb3);
           }
         else if (pat > 7 && pat < 12) {
           channel = 0;
           note =bass[slope];
-//          talkMIDI(0xB0, 0x0c, reverb2);
         }else
         {
           channel = 1;
           note = bass[slope]+12;
-//          talkMIDI(0xB0, 0x0c, reverb1);
         }
           noteOn(channel, note, 40 + (((instrument[12][pat] & (0x0f)>> tick) & (1)) * 20));
         }
